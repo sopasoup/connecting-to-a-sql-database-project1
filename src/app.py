e917643d88180681d98ca8628455891ae2c2eece
Aldo <157986984+sopasoup@users.noreply.github.com>
2025-01-16 12:57:42 +0000

    borrar requiriments

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 import pandas as pd
 from dotenv import load_dotenv
 
@@ -8,12 +8,18 @@ load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
 
-connection_string = f"postgresql://{os.getenv('gitpod')}:{os.getenv('postgres')}@{os.getenv('localhost')}/{os.getenv('sample_db')}"
+connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 engine = create_engine(connection_string).execution_options(autocommit=True)
-engine.connect()
+conn = engine.connect()
+
+conn.execute(text('''DROP TABLE IF EXISTS book_authors;
+               DROP TABLE IF EXISTS books;
+               DROP TABLE IF EXISTS authors;
+               DROP TABLE IF EXISTS publishers;
+               '''))
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
-engine.execute(""" CREATE TABLE publishers(
+conn.execute(""" CREATE TABLE publishers(
     publisher_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY(publisher_id)
@@ -45,11 +51,11 @@ CREATE TABLE book_authors (
     PRIMARY KEY(book_id, author_id),
     CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
     CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-);  """)
+); """)
 
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-engine.execute("""  -- publishers
+conn.execute("""  -- publishers
 INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
 INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
@@ -94,5 +100,5 @@ INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);   """)
 
 
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
-dataframe = pd.read_sql("Select * from books;", engine)
+dataframe = pd.read_sql("Select * from books;", conn)
 print(dataframe.describe())
