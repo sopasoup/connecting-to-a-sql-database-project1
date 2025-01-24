11271fca88fb1344b39ed920428b005dcae193c7
Aldo <157986984+sopasoup@users.noreply.github.com>
2025-01-24 14:02:35 +0000

    proyecto sql

--- src/app.py
+++ src/app.py
@@ -19,7 +19,7 @@ conn.execute(text('''DROP TABLE IF EXISTS book_authors;
                '''))
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
-conn.execute(""" CREATE TABLE publishers(
+conn.execute(text(""" CREATE TABLE publishers(
     publisher_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY(publisher_id)
@@ -51,11 +51,11 @@ CREATE TABLE book_authors (
     PRIMARY KEY(book_id, author_id),
     CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
     CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-); """)
+); """))
 
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
-conn.execute("""  -- publishers
+conn.execute(text("""  -- publishers
 INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
 INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
@@ -96,7 +96,7 @@ INSERT INTO book_authors (book_id, author_id) VALUES (6, 4);
 INSERT INTO book_authors (book_id, author_id) VALUES (7, 3);
 INSERT INTO book_authors (book_id, author_id) VALUES (8, 2);
 INSERT INTO book_authors (book_id, author_id) VALUES (9, 4);
-INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);   """)
+INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);   """))
 
 
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
